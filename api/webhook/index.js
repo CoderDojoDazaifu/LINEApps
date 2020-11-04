043ac36a06f2d92299301580f6de992a88e15f51
miki <masataka.miki@gmail.com>
2020-11-04 16:13:55 +0900

    オウム返しBot完成!

---
+++ api/webhook/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const createHandler = require("azure-function-express").createHandler;
+const express = require('express');
+
+const config = {
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+};
+
+const app = express();
+
+app.get('/api/webhook', (req, res) => res.send('Hello LINE BOT!(GET)')); // ブラウザ確認用(無くても問題ない)
+app.post('/api/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+
+    // ここのif文はdeveloper consoleの"接続確認"用なので削除して問題ない
+    if (req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff') {
+        res.send('Hello LINE BOT!(POST)');
+        console.log('疎通確認用');
+        return;
+    }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text // オウム返し
+    });
+}
+
+module.exports = createHandler(app);
